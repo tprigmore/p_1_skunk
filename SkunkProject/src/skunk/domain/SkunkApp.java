ca49f9ebb68421e17539101523ce59cddf1a7f52
Scott Prigmore <prig6030@stthomas.edu>
2020-10-07 16:27:31 -0500

    Initial commit

---
+++ SkunkProject/src/skunk/domain/SkunkApp.java
@@ -0,0 +1,23 @@
+package skunk.domain;
+
+import edu.princeton.cs.introcs.StdOut;
+
+public class SkunkApp
+{
+
+	public static void main(String[] args)
+	{
+		int value = 0 ;
+		StdOut.println("Welcome to the game skunk!");
+		
+		Dice dice = new Dice();
+		
+		dice.roll();
+		
+		value = dice.getLastRoll();
+		StdOut.println("The dice total is: " + value);
+		
+
+	}
+
+}
