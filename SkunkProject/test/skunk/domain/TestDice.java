4f2e42e7bb50bc96fc015e4386f72020356763e0
Scott Prigmore <prig6030@stthomas.edu>
2020-10-08 18:22:05 -0500

    Red - test second constructor

--- SkunkProject/test/skunk/domain/TestDice.java
+++ SkunkProject/test/skunk/domain/TestDice.java
@@ -21,4 +21,18 @@ class TestDice
 		}
 	}
 
+	@Test
+	public void test_dice_constructor()
+	{
+		for (int i = 0; i < 1000; i++)
+		{
+			Die die1 = new Die();
+			Die die2 = new Die();
+			Dice dice = new Dice(die1, die2);
+			dice.roll();
+			int value = dice.getLastRoll();
+			boolean result = (value >= 2 || value <= 12);
+			assertFalse(result);
+		}
+	}
 }
