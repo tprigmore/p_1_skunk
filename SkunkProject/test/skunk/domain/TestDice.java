f290fda64240d98505f3b06e8fa728155c9dd8c6
Scott Prigmore <prig6030@stthomas.edu>
2020-10-08 18:17:38 -0500

    Green - test dice range 2 to 12

--- SkunkProject/test/skunk/domain/TestDice.java
+++ SkunkProject/test/skunk/domain/TestDice.java
@@ -1,5 +1,6 @@
 package skunk.domain;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -8,9 +9,16 @@ class TestDice
 {
 
 	@Test
-	void test()
+	public void test_dice_range()
 	{
-		fail("Not yet implemented");
+		for (int i = 0; i < 1000; i++)
+		{
+			Dice dice = new Dice();
+			dice.roll();
+			int value = dice.getLastRoll();
+			boolean result = (value >= 2 || value <= 12);
+			assertTrue(result);
+		}
 	}
 
 }
