702e303831319dfc58b95b9021d8f9c14c0829df
Scott Prigmore <prig6030@stthomas.edu>
2020-10-08 14:03:46 -0500

    Green - Added function dice logic

--- SkunkProject/test/skunk/domain/PredictableDice.java
+++ SkunkProject/test/skunk/domain/PredictableDice.java
@@ -2,17 +2,27 @@ package skunk.domain;
 
 public class PredictableDice
 {
+	private int lastRoll;
+	private PredictableDie die1;
+	private PredictableDie die2;
+
+	public PredictableDice()
+	{
+		this.die1 = new PredictableDie(new int[] {1,2,3});
+		this.die2 = new PredictableDie(new int[] {1,2,3});
+	}
 
 	public void roll()
 	{
-
+		die1.roll();
+		die2.roll();
+		this.lastRoll = die1.getLastRoll() + die2.getLastRoll();
 		
 	}
 
 	public int getLastRoll()
 	{
-
-		return 2;
+		return this.lastRoll;
 	}
 
 }
