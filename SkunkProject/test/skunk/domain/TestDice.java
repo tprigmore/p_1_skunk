9a0c6614afb3e0a39a87493f29ff46b912bebea2
Scott Prigmore <prig6030@stthomas.edu>
2020-10-08 18:32:34 -0500

    Green - added tests for 3 to 12

--- SkunkProject/test/skunk/domain/TestDice.java
+++ SkunkProject/test/skunk/domain/TestDice.java
@@ -73,4 +73,187 @@ class TestDice
 		}
 		assertTrue(flag);
 	}
+	
+
+	@Test
+	public void test_dice_for_3()
+	{
+		Dice dice = new Dice();
+		int value;
+		boolean flag = false;
+		for (int i = 0; i < 1000; i++)
+		{
+			dice.roll();
+			value = dice.getLastRoll();
+			if (value == 3)
+			{
+				flag = true;
+				break;
+			}
+		}
+		assertTrue(flag);
+	}
+	@Test
+	public void test_dice_for_4()
+	{
+		Dice dice = new Dice();
+		int value;
+		boolean flag = false;
+		for (int i = 0; i < 1000; i++)
+		{
+			dice.roll();
+			value = dice.getLastRoll();
+			if (value == 4)
+			{
+				flag = true;
+				break;
+			}
+		}
+		assertTrue(flag);
+	}
+	@Test
+	public void test_dice_for_5()
+	{
+		Dice dice = new Dice();
+		int value;
+		boolean flag = false;
+		for (int i = 0; i < 1000; i++)
+		{
+			dice.roll();
+			value = dice.getLastRoll();
+			if (value == 5)
+			{
+				flag = true;
+				break;
+			}
+		}
+		assertTrue(flag);
+	}
+	@Test
+	public void test_dice_for_6()
+	{
+		Dice dice = new Dice();
+		int value;
+		boolean flag = false;
+		for (int i = 0; i < 1000; i++)
+		{
+			dice.roll();
+			value = dice.getLastRoll();
+			if (value == 6)
+			{
+				flag = true;
+				break;
+			}
+		}
+		assertTrue(flag);
+	}
+	@Test
+	public void test_dice_for_7()
+	{
+		Dice dice = new Dice();
+		int value;
+		boolean flag = false;
+		for (int i = 0; i < 1000; i++)
+		{
+			dice.roll();
+			value = dice.getLastRoll();
+			if (value == 7)
+			{
+				flag = true;
+				break;
+			}
+		}
+		assertTrue(flag);
+	}
+	@Test
+	public void test_dice_for_8()
+	{
+		Dice dice = new Dice();
+		int value;
+		boolean flag = false;
+		for (int i = 0; i < 1000; i++)
+		{
+			dice.roll();
+			value = dice.getLastRoll();
+			if (value == 8)
+			{
+				flag = true;
+				break;
+			}
+		}
+		assertTrue(flag);
+	}
+	@Test
+	public void test_dice_for_9()
+	{
+		Dice dice = new Dice();
+		int value;
+		boolean flag = false;
+		for (int i = 0; i < 1000; i++)
+		{
+			dice.roll();
+			value = dice.getLastRoll();
+			if (value == 9)
+			{
+				flag = true;
+				break;
+			}
+		}
+		assertTrue(flag);
+	}
+	@Test
+	public void test_dice_for_10()
+	{
+		Dice dice = new Dice();
+		int value;
+		boolean flag = false;
+		for (int i = 0; i < 1000; i++)
+		{
+			dice.roll();
+			value = dice.getLastRoll();
+			if (value == 10)
+			{
+				flag = true;
+				break;
+			}
+		}
+		assertTrue(flag);
+	}
+	@Test
+	public void test_dice_for_11()
+	{
+		Dice dice = new Dice();
+		int value;
+		boolean flag = false;
+		for (int i = 0; i < 1000; i++)
+		{
+			dice.roll();
+			value = dice.getLastRoll();
+			if (value == 11)
+			{
+				flag = true;
+				break;
+			}
+		}
+		assertTrue(flag);
+	}
+	@Test
+	public void test_dice_for_12()
+	{
+		Dice dice = new Dice();
+		int value;
+		boolean flag = false;
+		for (int i = 0; i < 1000; i++)
+		{
+			dice.roll();
+			value = dice.getLastRoll();
+			if (value == 12)
+			{
+				flag = true;
+				break;
+			}
+		}
+		assertTrue(flag);
+	}
+	
 }
