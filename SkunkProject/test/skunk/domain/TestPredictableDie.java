d90da610f63a52a1edaae4deb4fbcff53a1fecc6
Scott Prigmore <prig6030@stthomas.edu>
2020-10-07 19:08:20 -0500

    Red - Initial fail - created test_predictable_die_123

---
+++ SkunkProject/test/skunk/domain/TestPredictableDie.java
@@ -0,0 +1,24 @@
+package skunk.domain;
+
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPredictableDie
+{
+
+//	@Test
+//	void test()
+//	{
+//		fail("Not yet implemented");
+//	}
+	
+	@Test
+	public void test_predictable_die_123()
+	{
+		
+		fail("Not yet implemented");
+	}
+	
+}
