b61fea568d47e01f57329aab5dd419e79ca9ce03
Scott Prigmore <prig6030@stthomas.edu>
2020-10-08 13:42:32 -0500

    Red - first failing predictable dice test

---
+++ SkunkProject/test/skunk/domain/TestPredictableDice.java
@@ -0,0 +1,16 @@
+package skunk.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPredictableDice
+{
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
