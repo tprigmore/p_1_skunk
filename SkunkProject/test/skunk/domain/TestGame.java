3fa9a7a7d33d942b129ed2616d505e60749105c9
Scott Prigmore <prig6030@stthomas.edu>
2020-10-11 16:56:15 -0500

    Red - added tests for new classes.  All fail.

---
+++ SkunkProject/test/skunk/domain/TestGame.java
@@ -0,0 +1,16 @@
+package skunk.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestGame
+{
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
