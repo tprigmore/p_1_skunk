1d7111824c5d2b69152b3ad486bcfb4979bbcf47
Scott Prigmore <prig6030@stthomas.edu>
2020-10-11 21:53:34 -0500

    Red - test predictable turn

--- SkunkProject/test/skunk/domain/PredictableTurn.java
+++ SkunkProject/test/skunk/domain/PredictableTurn.java
@@ -1,16 +1,6 @@
 package skunk.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-class PredictableTurn
+public class PredictableTurn
 {
-
-	@Test
-	void test()
-	{
-		fail("Not yet implemented");
-	}
 
 }
---
+++ SkunkProject/test/skunk/domain/TestPredictableTurn.java
@@ -0,0 +1,16 @@
+package skunk.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPredictableTurn
+{
+
+	@Test
+	void test_predictable_turn()
+	{
+		TestPredictableTurn turn = new TestPredictableTurn();
+	}
+
+}
