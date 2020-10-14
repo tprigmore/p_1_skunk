330c89623844e5554a2d97eaaa70fc4fba0d9e0d
Scott Prigmore <prig6030@stthomas.edu>
2020-10-14 17:27:24 -0500

    Blue- refactored predictable dice test

--- SkunkProject/test/skunk/domain/TestPredictableDice.java
+++ SkunkProject/test/skunk/domain/TestPredictableDice.java
@@ -6,19 +6,22 @@ import org.junit.jupiter.api.Test;
 
 class TestPredictableDice
 {
+	private static final int SKUNK = -1;
+	private static final int SKUNK_DEUCE = -2;
+	private static final int DOUBLE_SKUNK = -4;
 
 	@Test
 	void test_predictable_dice_246()
 	{
 		PredictableDice dice = new PredictableDice();
 		dice.roll();
-		assertEquals(2, dice.getLastRoll());
+		assertEquals(DOUBLE_SKUNK, dice.getLastRoll());
 		dice.roll();
 		assertEquals(4, dice.getLastRoll());
 		dice.roll();
 		assertEquals(6, dice.getLastRoll());
 		dice.roll();
-		assertEquals(2, dice.getLastRoll());
+		assertEquals(DOUBLE_SKUNK, dice.getLastRoll());
 		dice.roll();
 		assertEquals(4, dice.getLastRoll());
 
