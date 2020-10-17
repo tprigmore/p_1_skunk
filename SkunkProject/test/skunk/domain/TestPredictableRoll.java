435bca8568f98c4e2e072fee0a17d3229b25af68
Scott Prigmore <prig6030@stthomas.edu>
2020-10-16 21:27:20 -0500

    Red - created predictable roll class test

---
+++ SkunkProject/test/skunk/domain/TestPredictableRoll.java
@@ -0,0 +1,16 @@
+package skunk.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPredictableRoll
+{
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
