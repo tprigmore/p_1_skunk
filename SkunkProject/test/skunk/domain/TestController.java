e89d51ededd31980e41b4ea8fba7fb37aa6619b0
Scott Prigmore <prig6030@stthomas.edu>
2020-10-23 12:51:09 -0500

    Green - added take a go to next player

--- SkunkProject/test/skunk/domain/TestController.java
+++ SkunkProject/test/skunk/domain/TestController.java
@@ -8,11 +8,34 @@ class TestController
 {
 
 	@Test
-	void test_controller_state()
+	void test_controller_add_and_get_player()
 	{
 		Controller controller = new Controller();
 		controller.addPlayer("Scott");
 		assertEquals("Scott", controller.getPlayerName());
 	}
+
+	@Test
+	void test_controller_get_running_total()
+	{
+		Controller controller = new Controller();
+		controller.addPlayer("Scott");
+		assertEquals(0, controller.getRunningTotal());
+	}
+	
+	@Test
+	void test_controller_go_to_next_player()
+	{
+		Controller controller = new Controller();
+		controller.addPlayer("Scott");
+
+		assertEquals("Scott", controller.getPlayerName());
+		controller.addPlayer("Joe");
+		controller.takeATurn();
+		controller.goToNextPlayer();
+		assertEquals("Joe", controller.getPlayerName());
+	}
+
+	
 
 }
